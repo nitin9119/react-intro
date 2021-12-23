abf4ccee1e6557312c21192798989204e6e807c8
nitin9119 <91542487+nitin9119@users.noreply.github.com>
2021-12-23 20:18:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  let array1 = ["Mobile Operating System","Android","Blackberry","Iphone","Windows Phone"];
+  let array2 = ["Mobile Manufacturer","Samsung","HTC","MicroMax","Apple"];
+  let arr1 = array1.slice(1);
+  let arr2 = array2.slice(1);
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {
+        <Todo1 head={array1[0]}/>        
+      }
+      {
+        arr1.map((e)=>(
+          <Todo2 list={e}/>
+        ))
+      }
+       {
+        <Todo1 head={array2[0]}/>        
+      }
+      {
+        arr2.map((e)=>(
+          <Todo2 list={e}/>
+        ))
+      }
+      
+      </div>
   );
+}
+
+function Todo1({head}){
+  return (
+   <h1> {head}</h1>
+  )
+}
+
+function Todo2({list}){
+  return (
+   <li> {list}</li>
+  )
 }
 
 export default App;
